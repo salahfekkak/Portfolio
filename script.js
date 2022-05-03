3d091d60a24f7da564b0f94785a2496435f89f0d
salahfekkak <fekkak.s@gmail.com>
2022-05-03 11:55:41 -0500

    Portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("#Paragraf").fadeIn(2500);
+    });
+  });
